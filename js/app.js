1d802cef1d2040238d2fa70de87527fb7d9e0b7a
Jamie Kawahara <kawaharajamie@gmail.com>
2018-09-11 18:52:54 -0700

    feat: Define Player function
    Set initial positions.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,43 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const Enemy = function(x=0, y=60) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 0;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.x = x;
+  this.y = y;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
-
+const Player = function(x=200, y=300) {
+  this.player = 'images/char-boy.png';
+  this.x = x;
+  this.y = y;
 }
 
 Player.prototype.update = function() {
 }
 
 Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.player), this.x, this.y);
 }
 
 Player.prototype.handleInput = function() {
@@ -45,18 +48,20 @@ Player.prototype.handleInput = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const enemy1 = new Enemy();
-var allEnemies = [enemy1];
-var player = new Player();
+const enemy2 = new Enemy(150, 180);
+const enemy3 = new Enemy(300, 140)
+const allEnemies = [enemy1, enemy2, enemy3];
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
