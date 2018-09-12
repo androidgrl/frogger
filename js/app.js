ec9b7e2794bd1571b123618bdd46b40638364f3e
Jamie Kawahara <kawaharajamie@gmail.com>
2018-09-11 18:27:40 -0700

    feat: Add Player function and basic methods

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 0;
 };
 
 // Update the enemy's position, required method for game
@@ -25,12 +27,26 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+
+}
+
+Player.prototype.update = function() {
+}
+
+Player.prototype.render = function() {
+}
+
+Player.prototype.handleInput = function() {
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const enemy1 = new Enemy();
+var allEnemies = [enemy1];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
